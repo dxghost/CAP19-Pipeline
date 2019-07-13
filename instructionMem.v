647f3a5b80ea2ac525299d4e9087c8c7d512fb75
mrphenomenon <mrezabaqery77@gmail.com>
2019-07-13 05:09:19 +0430

    Read instruction from txt

--- instructionMem.v
+++ instructionMem.v
@@ -4,37 +4,51 @@ module instructionMem (rst, addr, instruction);
   input rst;
   input [`WORD_LEN-1:0] addr;
   output [`WORD_LEN-1:0] instruction;
-
   wire [$clog2(`INSTR_MEM_SIZE)-1:0] address = addr[$clog2(`INSTR_MEM_SIZE)-1:0];
   reg [`MEM_CELL_SIZE-1:0] instMem [0:`INSTR_MEM_SIZE-1];
+
+  initial begin
+    $display("Loading rom.");
+    $readmemb("instruction.txt", instMem);
+  end
 
   always @ (*) begin
   	if (rst) begin
 
-        instMem[8] <=  4'b0011; //-- Add	A1,10
-        instMem[9] <= 4'b0001;
-        instMem[10]<= 4'b0000;
-        instMem[11]<= 4'b1010;
+        // instMem[0] <=  4'b0011; //-- Add	A1,10
+        // instMem[1] <= 4'b0001;
+        // instMem[2]<= 4'b0000;
+        // instMem[3]<= 4'b1010;
 
-        instMem[12] <= 4'b0011; //-- Add A2,11
-        instMem[13] <= 4'b0010;
-        instMem[14] <= 4'b0000;
-        instMem[15] <= 4'b1011;
+        // instMem[4] <= 4'b0011; //-- Add A2,11
+        // instMem[5] <= 4'b0010;
+        // instMem[6] <= 4'b0000;
+        // instMem[7] <= 4'b1011;
 
-        instMem[16] <= 4'b0011; //-- Add A3,3
-        instMem[17] <= 4'b0011;
-        instMem[18] <= 4'b0000;
-        instMem[19] <= 4'b0011;
+        // instMem[8] <=  4'b0011; //-- Add	A1,10
+        // instMem[9] <= 4'b0001;
+        // instMem[10]<= 4'b0000;
+        // instMem[11]<= 4'b1010;
 
-        instMem[20] <= 4'b1100; //-- MOV A3,17
-        instMem[21] <= 4'b0011;
-        instMem[22] <= 4'b0001;
-        instMem[23] <= 4'b0001;
+        // instMem[12] <= 4'b0011; //-- Add A2,11
+        // instMem[13] <= 4'b0010;
+        // instMem[14] <= 4'b0000;
+        // instMem[15] <= 4'b1011;
 
-        instMem[24] <= 4'b1001; //-- SW A3,25
-        instMem[25] <= 4'b0011;
-        instMem[26] <= 4'b0001;
-        instMem[27] <= 4'b1001;
+        // instMem[16] <= 4'b0011; //-- Add A3,3
+        // instMem[17] <= 4'b0011;
+        // instMem[18] <= 4'b0000;
+        // instMem[19] <= 4'b0011;
+
+        // instMem[20] <= 4'b1100; //-- MOV A3,17
+        // instMem[21] <= 4'b0011;
+        // instMem[22] <= 4'b0001;
+        // instMem[23] <= 4'b0001;
+
+        // instMem[24] <= 4'b1001; //-- SW A3,25
+        // instMem[25] <= 4'b0011;
+        // instMem[26] <= 4'b0001;
+        // instMem[27] <= 4'b1001;
 
 
         // instMem[24] <= 4'b0000; //-- Add A2,A3
@@ -42,10 +56,10 @@ module instructionMem (rst, addr, instruction);
         // instMem[26] <= 4'b0011; 17 + 11 = 28(11100)
         // instMem[27] <= 4'b0000;
 
-        instMem[28] <= 4'b0001; //-- AddB A2,25
-        instMem[29] <= 4'b0010;
-        instMem[30] <= 4'b0001;
-        instMem[31] <= 4'b1001;
+        // instMem[28] <= 4'b0001; //-- AddB A2,25
+        // instMem[29] <= 4'b0010;
+        // instMem[30] <= 4'b0001;
+        // instMem[31] <= 4'b1001;
 
 
         // instMem[24] <= 4'b1001; //-- SW A1, A2, 0
